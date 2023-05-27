8cb7bd9a62427b63da765aaa58879e55d8fb2894
Tayyab <tayyab.shahzad100000@gmail.com>
2023-05-27 22:58:31 +0500

    Lesson 1

---
+++ build/js/main.js
@@ -0,0 +1,10 @@
+"use strict";
+let username = "instructor";
+console.log("username", username);
+let a = 10;
+let b = "10";
+let c = false;
+// Can use tsc NoEmitOnError -w to watch for errors
+// Can use tsc --init to create a tsconfig.json file
+// Can use tsc -w to watch for changes
+// Can use tsc -w --outDir to watch for changes and output to a specific directory
---
+++ src/main.ts
@@ -0,0 +1,10 @@
+let username = "instructor";
+console.log("username", username);
+let a: number = 10;
+let b: string = "10";
+let c: boolean = false;
+
+// Can use tsc NoEmitOnError -w to watch for errors
+// Can use tsc --init to create a tsconfig.json file
+// Can use tsc -w to watch for changes
+// Can use tsc -w --outDir to watch for changes and output to a specific directory
