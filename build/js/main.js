c3bdfbf42b8e0176e9fc7f7041285ff8dba2f58b
Tayyab <tayyab.shahzad100000@gmail.com>
2023-05-27 23:57:12 +0500

    Lesson 3 Arrays and objects

--- build/js/main.js
+++ build/js/main.js
@@ -1,7 +1,36 @@
 "use strict";
-let a = 5, b = "6", c = true, d = "hello";
-let e = 5;
-let f;
-let reg = /hello/;
-const sum = (a, b) => a + b;
-console.log(sum(a, b));
+//Arrays and objects
+let stringArray = ["a", "b", "c"];
+let guitars = ["Gibson", "Fender", 5150];
+let mixedArray = ["Hello", 1, true];
+// stringArray[0] = 1; //Error because it is not a string
+guitars[3] = "Ibanez";
+mixedArray.unshift("First Item");
+console.log(mixedArray);
+mixedArray = stringArray;
+mixedArray = guitars;
+guitars = stringArray;
+// guitars = mixedArray //Error
+let myTuple = ["Hello", 1, true];
+// myTuple = mixedArray; //Error because it is not a tuple
+mixedArray = myTuple;
+console.log(mixedArray);
+// let myObj: object;
+let myObj = {};
+myObj = [];
+console.log(typeof myObj);
+let vanHalen = {
+    name: "Van Halen",
+    active: true,
+    albums: ["Van Halen", "1984", 5150],
+};
+let jp = {
+    name: "Jimmy",
+    active: false,
+    albums: ["I", "II", "5150"],
+};
+// jp = vanHalen; //Emit error if  remove any property from the type Guitarists if ? is not used in the property
+let greetGuitarist = (guitarist) => {
+    console.log(`Hello ${guitarist.name}`);
+};
+console.log(greetGuitarist(vanHalen));
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,59 @@
-let a: number = 5,
-  b: string = "6",
-  c: boolean = true,
-  d: any = "hello";
+//Arrays and objects
+let stringArray = ["a", "b", "c"];
 
-let e: number | string = 5;
+let guitars = ["Gibson", "Fender", 5150];
 
-let f: number[] | string[];
+let mixedArray = ["Hello", 1, true];
 
-let reg: RegExp = /hello/;
+// stringArray[0] = 1; //Error because it is not a string
 
-const sum = (a: number, b: string) => a + b;
+guitars[3] = "Ibanez";
 
-console.log(sum(a, b));
+mixedArray.unshift("First Item");
+console.log(mixedArray);
+
+mixedArray = stringArray;
+mixedArray = guitars;
+guitars = stringArray;
+// guitars = mixedArray //Error
+let myTuple: [string, number, boolean] = ["Hello", 1, true];
+
+// myTuple = mixedArray; //Error because it is not a tuple
+mixedArray = myTuple;
+console.log(mixedArray);
+
+// let myObj: object;
+let myObj = {};
+myObj = [];
+console.log(typeof myObj);
+
+// type Guitarists = {
+//   name: string;
+//   active?: boolean; //? means optional property
+//   albums: (string | number)[];
+// };
+
+interface Guitarists {
+  name: string;
+  active?: boolean; //? means optional property
+  albums: (string | number)[];
+}
+
+let vanHalen: Guitarists = {
+  name: "Van Halen",
+  active: true,
+  albums: ["Van Halen", "1984", 5150],
+};
+let jp: Guitarists = {
+  name: "Jimmy",
+  active: false,
+  albums: ["I", "II", "5150"],
+};
+
+// jp = vanHalen; //Emit error if  remove any property from the type Guitarists if ? is not used in the property
+
+let greetGuitarist = (guitarist: Guitarists) => {
+  console.log(`Hello ${guitarist.name}`);
+};
+
+console.log(greetGuitarist(vanHalen));
