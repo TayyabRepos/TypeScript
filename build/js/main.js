4c8af3dc784d84bb7dcb2026b6909b0e1d95f947
Tayyab <tayyab.shahzad100000@gmail.com>
2023-05-29 15:38:54 +0500

    Lesson 4 Functions

--- build/js/main.js
+++ build/js/main.js
@@ -1,36 +1,39 @@
 "use strict";
-//Arrays and objects
-let stringArray = ["a", "b", "c"];
-let guitars = ["Gibson", "Fender", 5150];
-let mixedArray = ["Hello", 1, true];
-// stringArray[0] = 1; //Error because it is not a string
-guitars[3] = "Ibanez";
-mixedArray.unshift("First Item");
-console.log(mixedArray);
-mixedArray = stringArray;
-mixedArray = guitars;
-guitars = stringArray;
-// guitars = mixedArray //Error
-let myTuple = ["Hello", 1, true];
-// myTuple = mixedArray; //Error because it is not a tuple
-mixedArray = myTuple;
-console.log(mixedArray);
-// let myObj: object;
-let myObj = {};
-myObj = [];
-console.log(typeof myObj);
-let vanHalen = {
-    name: "Van Halen",
-    active: true,
-    albums: ["Van Halen", "1984", 5150],
+//Functions
+//Literal types
+let user;
+// user = 'Wahab' //Error because user can only be Tayyab as it is a literal type
+let userName;
+userName = "Amtul"; //No error because userName can be any of the three values
+//Functions
+let sum = (a, b) => a + b;
+console.log(sum(1, 2));
+let multiply = function (a, b) {
+    return a * b;
 };
-let jp = {
-    name: "Jimmy",
-    active: false,
-    albums: ["I", "II", "5150"],
+console.log(multiply(2, 3));
+//Optional parameters
+let AddAll = (a, b, c) => {
+    if (typeof c !== "undefined") {
+        return a + b + c;
+    }
+    return a + b;
 };
-// jp = vanHalen; //Emit error if  remove any property from the type Guitarists if ? is not used in the property
-let greetGuitarist = (guitarist) => {
-    console.log(`Hello ${guitarist.name}`);
+//Rest Parameters
+let total = (a = 10, b = 2, ...num) => {
+    return a + b + num.reduce((prev, curr) => prev + curr);
 };
-console.log(greetGuitarist(vanHalen));
+console.log(total(13, 41, 5, 51, 12));
+//Never Type
+let errMsg = (errMsg) => {
+    throw new Error(errMsg);
+};
+let infiniteLoop = () => {
+    let i = 1;
+    while (true) {
+        i++;
+        if (i > 100)
+            break; // Come out from loop
+    }
+    return;
+};
--- src/main.ts
+++ src/main.ts
@@ -1,59 +1,70 @@
-//Arrays and objects
-let stringArray = ["a", "b", "c"];
+//Functions
 
-let guitars = ["Gibson", "Fender", 5150];
+//Type alias
 
-let mixedArray = ["Hello", 1, true];
+type StringOrNumber = string | number;
 
-// stringArray[0] = 1; //Error because it is not a string
+type StringOrNumberArray = (string | number)[];
 
-guitars[3] = "Ibanez";
-
-mixedArray.unshift("First Item");
-console.log(mixedArray);
-
-mixedArray = stringArray;
-mixedArray = guitars;
-guitars = stringArray;
-// guitars = mixedArray //Error
-let myTuple: [string, number, boolean] = ["Hello", 1, true];
-
-// myTuple = mixedArray; //Error because it is not a tuple
-mixedArray = myTuple;
-console.log(mixedArray);
-
-// let myObj: object;
-let myObj = {};
-myObj = [];
-console.log(typeof myObj);
-
-// type Guitarists = {
-//   name: string;
-//   active?: boolean; //? means optional property
-//   albums: (string | number)[];
-// };
-
-interface Guitarists {
+type Guitarists = {
   name: string;
   active?: boolean; //? means optional property
-  albums: (string | number)[];
-}
-
-let vanHalen: Guitarists = {
-  name: "Van Halen",
-  active: true,
-  albums: ["Van Halen", "1984", 5150],
-};
-let jp: Guitarists = {
-  name: "Jimmy",
-  active: false,
-  albums: ["I", "II", "5150"],
+  albums: StringOrNumberArray;
 };
 
-// jp = vanHalen; //Emit error if  remove any property from the type Guitarists if ? is not used in the property
+type UserId = StringOrNumber;
 
-let greetGuitarist = (guitarist: Guitarists) => {
-  console.log(`Hello ${guitarist.name}`);
+//Literal types
+let user: "Tayyab";
+
+// user = 'Wahab' //Error because user can only be Tayyab as it is a literal type
+
+let userName: "Tayyab" | "Amtul" | "Wahab";
+
+userName = "Amtul"; //No error because userName can be any of the three values
+
+//Functions
+
+let sum = (a: number, b: number): number => a + b;
+
+console.log(sum(1, 2));
+
+type MathFunction = (a: number, b: number) => number;
+
+let multiply: MathFunction = function (a: number, b: number): number {
+  return a * b;
 };
 
-console.log(greetGuitarist(vanHalen));
+console.log(multiply(2, 3));
+
+//Optional parameters
+
+let AddAll = (a: number, b: number, c?: number): number => {
+  if (typeof c !== "undefined") {
+    return a + b + c;
+  }
+  return a + b;
+};
+
+//Rest Parameters
+
+let total = (a: number = 10, b: number = 2, ...num: number[]): number => {
+  return a + b + num.reduce((prev, curr) => prev + curr);
+};
+
+console.log(total(13, 41, 5, 51, 12));
+
+//Never Type
+
+let errMsg = (errMsg: string) => {
+  throw new Error(errMsg);
+};
+
+let infiniteLoop = () => {
+  let i: number = 1;
+  while (true) {
+    i++;
+    if (i > 100) break; // Come out from loop
+  }
+  return;
+};
